e802bddd5b1c81f639d820bc9b047ff8d50affd3
翔太 大河原 <190462@jc-21.jp>
2021-11-16 12:55:51 +0900

    BMI計算式の追加

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -18,7 +18,29 @@ namespace R03BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            string height = heighit.Placeholder;
+            string weight = weighit.Placeholder;
+            try
+            {
+                double h = double.Parse(height);
+                double w = double.Parse(weight);
+                double b = w / h / h;
+                if (h >= 10)
+                {
+                    h = h / 100;
+                }
+                if (w >= 10)
+                {
+                    w = w / 100;
+                }
+                result.Text = "身長" + h + ",体重" + w + "の人のBMIは、" + 
+                    b + "です。";
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+                result.Text = "!!!";
+            }
         }
     }
 }
