3507eadcab353700ee4e8751fc7105b2b044bd7e
翔太 大河原 <190462@jc-21.jp>
2021-12-01 15:06:38 +0900

    計算式変更４

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -32,35 +32,24 @@ namespace R03BMI
                 {
                     w = w / 1000;
                 }
-                double b = Math.Round(w /(h*h),1);
+                double b = Math.Round(w / ( h * h ) , 1);
                 //int b2 = (int)b * 10;
                 //double b3 = (double)b2 / 10;
                 string s = null;
-                if (b < 18.5)
-                {
+                if (b < 18.5){
                     s = "低体重（痩せ）";
-                }
-                else if (b < 25.0)
-                {
+                }else if (b < 25.0){
                     s = "普通体重";
-                }
-                else if (b < 30.0)
-                {
+                }else if (b < 30.0){
                     s = "肥満（1度）";
-                }
-                else if (b < 35.0)
-                {
+                }else if (b < 35.0){
                     s = "肥満（2度）";
-                }
-                else if (b < 40.0)
-                {
+                }else if (b < 40.0){
                     s = "肥満（3度）";
-                }
-                else if (b >= 40.0)
-                {
+                }else{
                     s = "肥満（4度）";
                 }
-                result.Text =b + s;
+                result.Text ="BMI値は"+b+"で"+s+"です。";
             }
             catch (FormatException ex)
             {
